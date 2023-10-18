72da26069373efce1f2c43cbc84fee162598d2d6
MejseGK <s196050@dtu.dk>
2023-10-18 15:10:47 +0200

    concatenated data and correlation calculated hourly

---
+++ ConcatenateDataHourly.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 18 14:55:54 2023
+
+@author: MGRO0154
+"""
+
+
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 12 12:22:10 2023
+
+@author: ltei0004
+"""
+
+
+#%% Import packages
+import pandas as pd
+from datetime import datetime
+from ExtractIntervals import extract_one_night
+import numpy as np
+import os
+from ActiFeaturesFunc import *
+import csv
+from PlotFunc import *
+import plotly.io as io
+from ExtractIntervals import *
+import matplotlib.pyplot as plt
+
+
+#%% Set the base directory for the current family and session
+
+# Choose what study to import and preprocess data from
+#study = "MindYourDiabetes"
+# study = "Validationstudy_2020_2021_Cecilie"
+# study = "Sleep-1-child_2023_Cecilie"
+#study = "Kasper" 
+
+studies = ["MindYourDiabetes", "Sleep-1-child_2023_Cecilie", "Validationstudy_2020_2021_Cecilie"]
+
+#%% Initialize an empty list to store individual DataFrames
+dfs_cgm = []
+dfs_epochs = []
+
+for study in studies:
+    # Base directory/path
+    base_dir = os.path.join(r"L:\LovbeskyttetMapper01\StenoSleepQCGM", study)
+    
+    # List of families
+    families = [folder for folder in os.listdir(base_dir) if os.path.isdir(os.path.join(base_dir, folder))]
+    
+    # List of sessions
+    if study == "Validationstudy_2020_2021_Cecilie" or study == "Sleep-1-child_2023_Cecilie":
+        sessions = [None]
+    else:
+        folder_path = os.path.join(base_dir,families[0])
+        sessions = [folder for folder in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, folder))]
+    
+    
+    for family in families:
+         for session in sessions:
+            # Create path
+            if session==None:
+                # Construct the file path for the CSV data for the current family and session
+                cgm_path = os.path.join(base_dir, family, "cgm_data_processed_hourly_features.csv")
+                # Construct the file path for the CSV data for the current family and session
+                epoch_path = os.path.join(base_dir, family, "sleep_epochs_processed_hourly_features.csv")
+            else:
+                # Construct the file path for the CSV data for the current family and session
+                cgm_path = os.path.join(base_dir, family, session, "cgm_data_processed_hourly_features.csv")
+                # Construct the file path for the CSV data for the current family and session
+                epoch_path = os.path.join(base_dir, family, session, "sleep_epochs_processed_hourly_features.csv")
+             
+            
+             # Check if the file exists before reading it
+            if os.path.exists(cgm_path) and os.path.exists(epoch_path):
+                cgm_features = pd.read_csv(cgm_path, parse_dates=[0, 1], dayfirst=True)
+                epoch_features = pd.read_csv(epoch_path, parse_dates=[0, 1], dayfirst=True)
+            else:
+                print(f"Either/both epoch and CGM data file does not exist in {family}/{session}. Skipping...")
+                continue
+    
+            # Append the DataFrame to the list
+            dfs_cgm.append(cgm_features)
+    
+            # Append the DataFrame to the list
+            dfs_epochs.append(epoch_features)
+
+# Concatenate all the DataFrames in the list into one DataFrame
+epochs_concatenate = pd.concat(dfs_epochs, ignore_index=True)
+
+# Concatenate all the DataFrames in the list into one DataFrame
+cgm_concatenate = pd.concat(dfs_cgm, ignore_index=True)      
+
+
+#%% Export concatenated data to directory
+epochs_output_file = os.path.join(r"L:\LovbeskyttetMapper01\StenoSleepQCGM", "concatenated_hourly_epochs.csv")
+
+# Specify the filename for the concatenated CGM data
+cgm_output_file = os.path.join(r"L:\LovbeskyttetMapper01\StenoSleepQCGM", "concatenated_hourly_cgm.csv")
+
+# Export concatenated data to the specified files
+epochs_concatenate.to_csv(epochs_output_file, index=False)
+cgm_concatenate.to_csv(cgm_output_file, index=False)
--- CorrelationMatrix.py
+++ CorrelationMatrix.py
@@ -5,7 +5,8 @@ Created on Thu Oct 12 13:19:52 2023
 @author: ltei0004
 """
 
-#%% Import packages
+#%% Nightly
+
 import pandas as pd
 from datetime import datetime
 import os
@@ -23,10 +24,8 @@ epoch_file_path = os.path.join(base_dir+epoch_file)
 # Read the cgm and epoch data
 cgm_data = pd.read_csv(cgm_file_path)
 epoch_data = pd.read_csv(epoch_file_path)
-epoch_data.replace('Very bad sleep', 0, inplace=True)
-epoch_data.replace('Fairly bad sleep', 1, inplace=True)
-epoch_data.replace('Fairly good sleep', 2, inplace=True)
-epoch_data.replace('Very good sleep', 3, inplace=True)
+epoch_data.replace('Bad', 0, inplace=True)
+epoch_data.replace('Good', 1, inplace=True)
 
 data = pd.merge(cgm_data, epoch_data, on=["In Bed DateTime", "Out Bed DateTime"])
 
@@ -39,6 +38,44 @@ plt.get_current_fig_manager().full_screen_toggle()
 
 
 # Save plot 
-plt.savefig("H:\GitHub\Bachelor\Plots\corrMatrix.png", format="png")
+plt.savefig("H:\GitHub\Bachelor\Plots\corrMatrixy.png", format="png")
 
 plt.show()
+
+#%% Hourly
+
+import pandas as pd
+from datetime import datetime
+import os
+import seaborn as sn
+import matplotlib.pyplot as plt
+
+base_dir = r"L:\LovbeskyttetMapper01\StenoSleepQCGM\MindYourDiabetes"
+cgm_file = "\concatenated_hourly_cgm.csv"
+epoch_file = "\concatenated_hourly_epochs.csv"
+
+# Construct the full file paths
+cgm_file_path = os.path.join(base_dir+cgm_file)
+epoch_file_path = os.path.join(base_dir+epoch_file)
+
+# Read the cgm and epoch data
+cgm_data = pd.read_csv(cgm_file_path)
+epoch_data = pd.read_csv(epoch_file_path)
+epoch_data.replace('Bad', 0, inplace=True)
+epoch_data.replace('Good', 1, inplace=True)
+
+data = pd.merge(cgm_data, epoch_data, on=["DateTime start"])
+
+corrMatrix = data.corr()
+
+plt.figure()
+sn.heatmap(corrMatrix, annot=True)
+
+plt.get_current_fig_manager().full_screen_toggle()   
+
+
+# Save plot 
+plt.savefig("H:\GitHub\Bachelor\Plots\corrMatrix_hourly.png", format="png")
+
+plt.show()
+
