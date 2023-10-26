790aae00b52399965638b710a1db8b7b0c0d553f
MejseGK <s196050@dtu.dk>
2023-10-26 11:05:29 +0200

    Gen error and CV for hourly data

--- LinearRegression.py
+++ LinearRegression.py
@@ -73,7 +73,7 @@ print(model_nightly.summary())
 print(model_hourly.summary())
 
 
-#%% Linear Regression model (in another way)
+#%% Linear Regression model (in another way) (nightly)
 
 # Fit model
 model_lm = lm.LinearRegression()
@@ -125,7 +125,59 @@ plt.figure()
 plt.hist(residual_baseline, bins=30)
 plt.title(f"Histogram of the residual for {y.columns[0]} (baseline)")
 
-#%% Cross-validation to estimate generalization error
+#%% Linear Regression model (in another way) (hourly)
+
+# Fit model
+model_lm = lm.LinearRegression()
+model_lm.fit(xh_stan, yh_stan)
+
+#Estimate y values 
+yh_est = model_lm.predict(xh_stan)
+
+# Calculate residuals
+residual = yh_est - yh_stan
+
+#Scatter plot
+data_min = -3
+data_max = 4
+
+plt.figure()
+plt.plot(yh_stan, yh_est, ".")
+plt.gca().set_aspect('equal')
+plt.xlim(data_min, data_max)
+plt.ylim(data_min, data_max)
+plt.xlabel("True")
+plt.ylabel("Estimated")
+plt.title(f"Estimated vs true values for hourly {y.columns[0]}")
+
+# Histogram of residuals
+plt.figure()
+plt.hist(residual, bins=30)
+plt.title(f"Histogram of the residual for hourly {y.columns[0]}")
+
+# Print model coefficients
+print(model_lm.intercept_)
+print(model_lm.coef_)
+
+# Plot baseline model (mean of target variable)
+yh_mean = yh_stan.iloc[:]['WASO'].mean()
+yh_baseline = pd.DataFrame({'WASO': [yh_mean] * len(yh_stan)})
+plt.figure()
+plt.plot(yh_stan, yh_baseline, ".")
+plt.gca().set_aspect('equal')
+plt.xlim(data_min, data_max)
+plt.ylim(data_min, data_max)
+plt.xlabel("True")
+plt.ylabel("Estimated")
+plt.title(f"Estimated vs true values for hourly {y.columns[0]} (baseline)")
+
+# Histogram of residuals
+residual_baseline = yh_baseline - yh_stan
+plt.figure()
+plt.hist(residual_baseline, bins=30)
+plt.title(f"Histogram of the residual for hourly {y.columns[0]} (baseline)")
+
+#%% Cross-validation to estimate generalization error (nightly)
 
 # Extract x and y values from dataframes
 X = x_stan.values
@@ -171,5 +223,49 @@ for train_index, test_index in CV.split(X):
 est_gen_error = Error_test.mean() 
 est_gen_error_baseline = Error_test_baseline.mean()
 
+#%% Cross-validation to estimate generalization (hourly)
 
+# Extract x and y values from dataframes
+X = xh_stan.values
+Y = yh_stan.values 
+
+# Create crossvalidation partition for evaluation
+K = 10
+CV = model_selection.KFold(n_splits=K,shuffle=True)
+
+# Create empty arrays for storing test and training errors
+Error_train = np.empty(K)
+Error_test = np.empty(K)
+Error_train_baseline = np.empty((K,1))
+Error_test_baseline = np.empty((K,1))
+
+k=0
+for train_index, test_index in CV.split(X):
+    print('Computing CV fold: {0}/{1}..'.format(k+1,K))
+
+    # Extract training and test set for current CV fold
+    X_train, y_train = X[train_index,:], Y[train_index]
+    X_test, y_test = X[test_index,:], Y[test_index]
+    
+    # Fit linear regression model
+    model_lm = lm.LinearRegression()
+    model_lm.fit(X_train, y_train)
+
+    # Predict
+    y_est_train = model_lm.predict(X_train) 
+    y_est_test = model_lm.predict(X_test)   
+    
+    # MSE of baseline model (mean of target variable)
+    Error_train_baseline[k] = np.square(y_train-y_train.mean()).sum()/y_train.shape[0]
+    Error_test_baseline[k] = np.square(y_test-y_test.mean()).sum()/y_test.shape[0]
+    
+    # MSE of linear regression model
+    Error_train[k] = np.square(y_train-y_est_train).sum()/y_train.shape[0]
+    Error_test[k] = np.square(y_test-y_est_test).sum()/y_test.shape[0]     
+ 
+    k+=1
+
+# Estimate generalization errors
+est_gen_error = Error_test.mean() 
+est_gen_error_baseline = Error_test_baseline.mean()
 
