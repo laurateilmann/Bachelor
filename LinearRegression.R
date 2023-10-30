9d9846f56d0bd286a674cd4600157f921e3661f2
MejseGK <s196050@dtu.dk>
2023-10-30 15:12:10 +0100

    Linear regression with random effect for ID (in R)

---
+++ LinearRegression.R
@@ -0,0 +1,42 @@
+
+# Load libraries
+library(dplyr)
+library(readr)
+library(zoo)
+library(lme4)
+
+# Set your base directory
+base_dir <- "L:/LovbeskyttetMapper01/StenoSleepQCGM"
+
+# Load the nightly data
+complete_dataset <- read_csv(file.path(base_dir, 'concatenated_all.csv'), col_types = cols())
+
+# Handling missing and infinite values
+complete_dataset <- na.omit(complete_dataset)
+
+# Define the independent and dependent variables
+x <- complete_dataset %>% select(3:12)
+y <- complete_dataset %>% select(Efficiency)
+
+# Standardize data
+x_stan <- scale(x, center = TRUE, scale = TRUE)
+y_stan <- scale(y, center = TRUE, scale = TRUE)
+
+# Include offset
+x_stan <- cbind(1, x_stan)
+
+# Extract ID
+Id <- complete_dataset %>% select(id)
+
+# Create a new data frame combining x_stan and y_stan
+standardized_data <- data.frame(x_stan, y_stan, id=Id)
+
+# Model
+model <- lmer(Efficiency ~ TIR + TAR + cv + (1 | id), data = standardized_data)
+summary(model)
+
+# p-value
+p = round(2*pnorm(abs(coef(summary(model))[,3]), lower.tail = FALSE),3)
+print(p)
+
+
