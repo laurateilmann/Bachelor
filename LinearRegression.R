7525e0b6bccbf4d96d2ff2192f9a0255d15deef8
MejseGK <s196050@dtu.dk>
2023-11-06 21:40:01 +0100

    Notes added

--- LinearRegression.R
+++ LinearRegression.R
@@ -8,7 +8,7 @@ library(stats)
 library(olsrr)
 
 # Set your base directory
-base_dir <- "L:/LovbeskyttetMapper01/StenoSleepQCGM"
+base_dir <- "L:/LovbeskyttetMapper01/StenoSleepQCGM/Concatenated data"
 
 # Load the nightly data
 complete_dataset <- read_csv(file.path(base_dir, 'concatenated_all.csv'), col_types = cols())
@@ -18,7 +18,7 @@ complete_dataset <- na.omit(complete_dataset)
 
 # Define the independent and dependent variables
 x <- complete_dataset %>% select(3:12)
-y <- complete_dataset %>% select(WASO)
+y <- complete_dataset %>% select(Efficiency)
 
 # Standardize data
 x_stan <- scale(x, center = TRUE, scale = TRUE)
@@ -34,12 +34,14 @@ Id <- complete_dataset %>% select(id)
 standardized_data <- data.frame(x_stan, y_stan, id=Id)
 
 # Model
-model <- lmer(WASO ~ TIR + TAR + TBR + min + max + mean + median + std + cv + delta.IG + (1 | id), data = standardized_data)
+model <- lmer(Efficiency ~ TAR + TBR + min + max + mean + median + std + cv + delta.IG + (1 | id), data = standardized_data)
 summary(model)
 
 # p-value
 p = round(2*pnorm(abs(coef(summary(model))[,3]), lower.tail = FALSE),3)
 print(p)
+
+plot(standardized_data)
 
 #################
 
@@ -47,7 +49,22 @@ print(p)
 modelf <- lm(WASO ~ TIR + TAR + min + max + mean + std + delta.IG, data = standardized_data)
 final_model <- ols_step_all_possible(modelf)
 print(final_model)
- 
+
+
+# Noter:
+
+# AIC and BIC
+
+# Kig på hver variabel alene op mod WASO (husk intercept)
+
+# Fjern en variabel ad gangen (der ikke er signifikant) indtil alle er signifikante. Derefter lav anova mellem den model vi er nået frem til og den oprindelige model med alle variable, for at se om de er signifikant forskellige.
+
+# Cluster matrix med fx pairwise p-værdier 
+
+# Enten machine learning eller statistik vej
+
+
+
 
 
 
