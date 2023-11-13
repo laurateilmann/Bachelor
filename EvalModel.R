421730c35048e7efc47d964fef941b6f9539026c
MejseGK <s196050@dtu.dk>
2023-11-13 13:14:16 +0100

    statistical comparison working correctly now

--- EvalModel.R
+++ EvalModel.R
@@ -42,11 +42,96 @@ Id <- complete_dataset %>% select(id)
 # Standardize data
 x_stan <- scale(x, center = TRUE, scale = TRUE)
 
-# Include offset
-x_stan <- cbind(1, x_stan)
-
 # Create a new data frame combining x_stan and y_stan
 X <- data.frame(x_stan, y, id=Id)
+
+
+###########################################################
+
+## K-fold cross-validation
+
+# Set seed for reproducibility. 
+set.seed(7645)
+
+# Number of times cross-validation is performed
+J = 1
+# Number of folds in K-fold cross-validation
+K = 5
+
+# Initialize lists and vectors
+y_true <- c()
+y_model <- c()
+y_baseline <- c()
+p_values_list <- vector("list", K)
+estimates_list <- vector("list", K)
+CI_list <- vector("list",K)
+
+
+# Outer loop - number of times cross-validation and t-test is performed
+for (j in 1:J) {
+  
+  # Create folds
+  CV <- list()
+  CV$which <- createFolds(1:N, k = K, list = F)
+  
+  # Inner loop - cross-validation
+  for (k in 1:K) {
+  
+    # Extract training and test set
+    X_train <- X[CV$which != k, ]
+    X_test <- X[CV$which == k, ]
+    
+    # Train model
+    model <- lmer("WASO ~ cv + (1 | id)", data = X_train)
+  
+    # Model predictions
+    y_est_model <- predict(model, X_test)
+  
+    # Baseline model predictions
+    mean_value <- mean(X_test$WASO)
+    y_est_baseline <- data.frame(y_est_baseline = rep(mean_value, nrow(X_test)))
+  
+    # The true output values
+    y_test <- X_test[11]
+  
+    # Save results
+    y_model <- c(y_model, list(y_est_model))
+    y_baseline <- c(y_baseline, y_est_baseline)
+    y_true <- c(y_true, y_test)
+    
+  }
+  
+  # Convert lists to vectors
+  y_model <- unlist(y_model)
+  y_baseline <- unlist(y_baseline)
+  y_true <- unlist(y_true)
+
+  # Compute loss
+  z_model <- abs(y_true - y_model)**2
+  z_baseline <- abs(y_true - y_baseline)**2
+  # Compute difference in loss
+  z <- z_model - z_baseline
+  
+  # Compute p-value and CI for the differences between models
+  t_test_result <- t.test(z, alternative = "two.sided", alpha = 0.05)
+  
+  # Save p-value and CI
+  p_values_list[[j]] <- t_test_result$p.value
+  estimates_list[[j]] <- t_test_result$estimate
+  CI_list[[j]] <- t_test_result$conf.int
+  
+}
+
+# Print result
+for (j in 1:J) {
+  cat(
+    "Iteration", j, 
+    ", estimate:", estimates_list[[j]],
+    ", p-value:", p_values_list[[j]], 
+    ", CI: [", CI_list[[j]][1], ",", CI_list[[j]][2], "]", 
+    "\n"
+  )
+}
 
 #########################################################
 
@@ -111,56 +196,3 @@ X <- data.frame(x_stan, y, id=Id)
 
 
 
-
-
-
-###########################################################
-
-## K-fold cross-validation
-
-J = 10
-K = 5
-
-r <- matrix(NA, 0, 1)
-p_values_list <- vector("list", K)
-estimates_list <- vector("list", K)
-
-for (j in 1:J) {
-  
-  CV <- list()
-  CV$which <- createFolds(1:N, k = K, list = F)
-  
-  for (k in 1:K) {
-  
-    # Extract training and test set
-    X_train <- X[CV$which != k, ]
-    X_test <- X[CV$which == k, ]
-  
-    model <- lmer("WASO ~ cv + (1 | id)", data = X_train)
-  
-    y_est_model <- predict(model, X_test)
-  
-    mean_value <- mean(X_test$WASO)
-    y_est_baseline <- data.frame(y_est_baseline = rep(mean_value, nrow(X_test)))
-  
-    y_test <- X_test[12]
-  
-    zA <- abs(y_test - y_est_model)**2
-    zB <- abs(y_test - y_est_baseline)**2
-    z <- zA - zB
-  
-    r <- rbind(r, mean(z))
-  }
-  
-  # Compute confidence interval and p-value for the differences between models
-  t_test_result <- t.test(r, alternative = "two.sided", alpha = 0.05)
-  
-  p_values_list[[j]] <- t_test_result$p.value
-  estimates_list[[j]] <- t_test_result$estimate
-
-}
-
-# Print result
-for (j in 1:J) {
-  cat("Iteration", j, ":", "p-value:", p_values_list[[j]], ", estimate:", estimates_list[[j]], "\n")
-}
--- EvalModelHourly.R
+++ EvalModelHourly.R
@@ -20,9 +20,9 @@ base_dir <- "L:/LovbeskyttetMapper01/StenoSleepQCGM/Concatenated data"
 # Variable names
 var_names <- list("TIR", "TAR", "TBR", "min", "max", "mean", "median", "std", "cv", "delta.IG")
 
-### Nightly
+### Hourly
 
-# Load the nightly data
+# Load the hourly data
 complete_dataset <- read_csv(file.path(base_dir, 'concatenated_hourly_all_11.csv'), col_types = cols())
 
 # Handling missing and infinite values
@@ -32,6 +32,7 @@ complete_dataset <- na.omit(complete_dataset)
 x <- complete_dataset %>% select(2:11)
 y <- complete_dataset %>% select(WASO)
 
+# Dimensions of x
 N <- nrow(x)
 M <- ncol(x)
 
@@ -41,136 +42,134 @@ Id <- complete_dataset %>% select(id)
 # Standardize data
 x_stan <- scale(x, center = TRUE, scale = TRUE)
 
-# Include offset
-x_stan <- cbind(1, x_stan)
-
 # Create a new data frame combining x_stan and y_stan
 X <- data.frame(x_stan, y, id=Id)
 
-#########################################################
-
-## Hold-out cross-validation
-
-## Set the seed to make your partition reproducible
-set.seed(123)
-train_ind <- createDataPartition(y = X$WASO, p = 0.7, list = FALSE, groups = length(unique(X$id)))
-
-X_train <- X[train_ind, ]
-X_test <- X[-train_ind, ]
-
-model <- lmer("WASO ~ cv + (1 | id)", data = X_train)
-
-y_est_model <- predict(model, X_test)
-mean_value <- mean(X_test$WASO)
-y_est_baseline <- data.frame(y_est_baseline = rep(mean_value, nrow(X_test)))
-
-y_test <- X_test[12]
-
-
-# Compute z with squared error.
-zA <- abs(y_test - y_est_model)**2
-zB <- abs(y_test - y_est_baseline)**2
-z <- zA - zB
-
-# Confidence interval for model A (model)
-res <- t.test(zA, alternative = "two.sided", alpha = 0.05)
-(CIA <- c(res$conf.int[1], res$conf.int[2]))
-
-# Confidence interval for model B (baseline)
-res <- t.test(zA, alternative = "two.sided", alpha = 0.05)
-(CIA <- c(res$conf.int[1], res$conf.int[2]))
-
-# Compute confidence interval of z = zA-zB and p-value of Null hypothesis
-
-
-t.test(z, alternative = "two.sided", alpha = 0.05)
 
 ###########################################################
 
 ## K-fold cross-validation
 
-r <- matrix(NA, 0, 1)
+# Set seed for reproducibility 
+set.seed(7645)
 
+# Number of times cross-validation is performed
+J = 1
+# Number of folds in K-fold cross-validation
 K = 5
-CV <- list()
-CV$which <- createFolds(1:N, k = K, list = F)
 
-for (k in 1:K) {
+# Initialize lists and vectors
+y_true <- c()
+y_model <- c()
+y_baseline <- c()
+p_values_list <- vector("list", K)
+estimates_list <- vector("list", K)
+CI_list <- vector("list",K)
+
+# Outer loop - number of times cross-validation and t-test is performed
+for (j in 1:J) {
   
-  # Extract training and test set
-  X_train <- X[CV$which != k, ]
-  X_test <- X[CV$which == k, ]
+  # Create folds
+  CV <- list()
+  CV$which <- createFolds(1:N, k = K, list = F)
   
-  model <- lmer("WASO ~ cv + (1 | id)", data = X_train)
+  # Inner loop - cross-validation
+  for (k in 1:K) {
+    
+    # Extract training and test set
+    X_train <- X[CV$which != k, ]
+    X_test <- X[CV$which == k, ]
+    
+    # Train model
+    model <- lmer("WASO ~ cv + (1 | id)", data = X_train)
+    
+    # Model predictions
+    y_est_model <- predict(model, X_test)
+    
+    # Baseline model predictions
+    mean_value <- mean(X_test$WASO)
+    y_est_baseline <- data.frame(y_est_baseline = rep(mean_value, nrow(X_test)))
+    
+    # The true output values
+    y_test <- X_test[11]
+    
+    # Save results
+    y_model <- c(y_model, list(y_est_model))
+    y_baseline <- c(y_baseline, y_est_baseline)
+    y_true <- c(y_true, y_test)
+    
+  }
   
-  y_est_model <- predict(model, X_test)
+  # Convert lists to vectors
+  y_model <- unlist(y_model)
+  y_baseline <- unlist(y_baseline)
+  y_true <- unlist(y_true)
   
-  mean_value <- mean(X_test$WASO)
-  y_est_baseline <- data.frame(y_est_baseline = rep(mean_value, nrow(X_test)))
+  # Compute loss
+  z_model <- abs(y_true - y_model)**2
+  z_baseline <- abs(y_true - y_baseline)**2
+  # Compute difference in loss
+  z <- z_model - z_baseline
   
-  y_test <- X_test[12]
+  # Compute p-value and CI for the differences between models
+  t_test_result <- t.test(z, alternative = "two.sided", alpha = 0.05)
   
-  zA <- abs(y_test - y_est_model)**2
-  zB <- abs(y_test - y_est_baseline)**2
-  z <- zA - zB
+  # Save p-value and CI
+  p_values_list[[j]] <- t_test_result$p.value
+  estimates_list[[j]] <- t_test_result$estimate
+  CI_list[[j]] <- t_test_result$conf.int
   
-  r <- rbind(r, z)
+}
+
+# Print result
+for (j in 1:J) {
+  cat(
+    "Iteration", j, 
+    ", estimate:", estimates_list[[j]],
+    ", p-value:", p_values_list[[j]], 
+    ", CI: [", CI_list[[j]][1], ",", CI_list[[j]][2], "]", 
+    "\n"
+  )
 }
 
 
-# Compute confidence interval and p-value for the differences between models
-t.test(r, alternative = "two.sided", alpha = 0.05)
+#########################################################
 
-#################################################3
-
-# ## K-fold cross-validation
+# ## Hold-out cross-validation
 # 
-# J = 10
-# K = 5
+# ## Set the seed to make your partition reproducible
+# set.seed(123)
+# train_ind <- createDataPartition(y = X$WASO, p = 0.7, list = FALSE, groups = length(unique(X$id)))
 # 
-# r <- matrix(NA, 0, 1)
-# p_values_list <- vector("list", K)
-# estimates_list <- vector("list", K)
+# X_train <- X[train_ind, ]
+# X_test <- X[-train_ind, ]
 # 
-# for (j in 1:J) {
-#   
-#   CV <- list()
-#   CV$which <- createFolds(1:N, k = K, list = F)
-#   
-#   for (k in 1:K) {
-#     
-#     # Extract training and test set
-#     X_train <- X[CV$which != k, ]
-#     X_test <- X[CV$which == k, ]
-#     
-#     model <- lmer("WASO ~ cv + (1 | id)", data = X_train)
-#     
-#     y_est_model <- predict(model, X_test)
-#     
-#     mean_value <- mean(X_test$WASO)
-#     y_est_baseline <- data.frame(y_est_baseline = rep(mean_value, nrow(X_test)))
-#     
-#     y_test <- X_test[12]
-#     
-#     zA <- abs(y_test - y_est_model)**2
-#     zB <- abs(y_test - y_est_baseline)**2
-#     z <- zA - zB
-#     
-#     r <- rbind(r, z)
-#   }
-#   
-#   # Compute confidence interval and p-value for the differences between models
-#   t_test_result <- t.test(r, alternative = "two.sided", alpha = 0.05)
-#   
-#   p_values_list[[j]] <- t_test_result$p.value
-#   estimates_list[[j]] <- t_test_result$estimate
-#   
-# }
+# model <- lmer("WASO ~ cv + (1 | id)", data = X_train)
 # 
-# # Print result
-# for (j in 1:J) {
-#   cat("Iteration", j, ":", "p-value:", p_values_list[[j]], ", estimate:", estimates_list[[j]], "\n")
-# }
+# y_est_model <- predict(model, X_test)
+# mean_value <- mean(X_test$WASO)
+# y_est_baseline <- data.frame(y_est_baseline = rep(mean_value, nrow(X_test)))
+# 
+# y_test <- X_test[12]
+# 
+# 
+# # Compute z with squared error.
+# zA <- abs(y_test - y_est_model)**2
+# zB <- abs(y_test - y_est_baseline)**2
+# z <- zA - zB
+# 
+# # Confidence interval for model A (model)
+# res <- t.test(zA, alternative = "two.sided", alpha = 0.05)
+# (CIA <- c(res$conf.int[1], res$conf.int[2]))
+# 
+# # Confidence interval for model B (baseline)
+# res <- t.test(zA, alternative = "two.sided", alpha = 0.05)
+# (CIA <- c(res$conf.int[1], res$conf.int[2]))
+# 
+# # Compute confidence interval of z = zA-zB and p-value of Null hypothesis
+# 
+# 
+# t.test(z, alternative = "two.sided", alpha = 0.05)
 
 
 
